c263cbfd5ad5d85e503187151c6ac7ce89c6120a
Nicolas Sallerin <nicolas.sallerin@gmail.com>
2019-02-26 12:14:47 +0100

    foreign-key-added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_101135) do
+ActiveRecord::Schema.define(version: 2019_02_26_101136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_02_26_101135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
