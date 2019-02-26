1542c1d2073f662d1713d00ab7076bc9155a3d8a
Nicolas Sallerin <nicolas.sallerin@gmail.com>
2019-02-26 13:30:57 +0100

    Profile  pic added to Profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_110449) do
+ActiveRecord::Schema.define(version: 2019_02_26_122822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_110449) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_pic"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
