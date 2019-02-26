44d92bbb720dacb0550a40b1fc67a91eec76a1c0
Heloise Mazingarbe <heloise.mazingarbe@gmail.com>
2019-02-26 14:48:18 +0100

    Pairs Controller & associated methods created

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_123235) do
-
+ActiveRecord::Schema.define(version: 2019_02_26_123600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
