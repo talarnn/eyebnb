967c43882ec386f1160b3c07e67e1a933c957bb3
Heloise Mazingarbe <heloise.mazingarbe@gmail.com>
2019-02-26 11:41:00 +0100

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pairs#index'
+  resources :pairs, except: [ :index ] do 
+    resources :bookings, only: [ :new, :create ]
+  end
+  patch '/booking/:id', to: "bookings#update"
+  delete '/booking/:id', to: "bookings#destroy"
+  resources :profiles, only: [ :show, :dashboard, :edit, :update ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
