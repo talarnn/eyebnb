abcd09fe4182b722b4aafc0641d867440ec45f09
Joseph Wiel <joseph@comet.co>
2019-02-28 11:23:39 +0100

    reviewed structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-
   root to: 'pairs#index'
   resources :pairs, except: [ :index ] do
     resources :bookings, only: [ :new, :create ]
